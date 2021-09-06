ebd76883a415596694c8515480fbd76cd7c940e3
KATSIARYNA <statsevich22@gmail.com>
2021-09-06 15:58:13 +0300

    refactor: change code

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,7 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
+export default function countCats(matrix) {
   const arr = [];
   const ears = '^^';
 
